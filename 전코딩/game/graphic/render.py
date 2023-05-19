10bd2512028ab66bcee619d783dc1ed8d4c0dc9b
ironman <124940799+zzipper21@users.noreply.github.com>
2023-05-20 00:46:15 +0900

    Delete 전코딩 directory

--- 전코딩/game/graphic/render.py
+++
@@ -1,4 +0,0 @@
-from ..sound.echo import echo_test
-def render_test():
-    print('render')
-    echo_test()
--- 전코딩/mod1.py
+++
@@ -1,8 +0,0 @@
-def add(a,b):
-    return a+b
-def sub(a,b):
-    return a-b
-
-if __name__=='__main__':
-    print(add(8,4))
-    print(sub(8,4))
